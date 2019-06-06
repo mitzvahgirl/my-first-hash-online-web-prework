fd1ea2156142f1fcfbde6c513d389553c58a0a73
Amy Fogelman <abfnj1@gmail.com>
2019-06-06 16:53:45 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -18,7 +18,8 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+  # Look up the value of the "oil paintings" key in the
+    shipping_manifest hash below
 shipping_manifest["oil paintings"]
 end
 
